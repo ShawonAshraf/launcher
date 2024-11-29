34c8e70f0b3cd769f4ad89257c38c3d71a77a9af
Shawon Ashraf <7804029+ShawonAshraf@users.noreply.github.com>
2024-11-29 10:43:30 +0100

    basic table layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-// import {useState} from "react";
-// import {invoke} from "@tauri-apps/api/core";
+import {useState, useEffect} from "react";
+import {invoke} from "@tauri-apps/api/core";
 import "./App.css";
 
+interface Executable {
+    id: number;
+    name: string;
+    path: string;
+}
+
 function App() {
+    const [executables, setExecutables] = useState([]);
+
+    useEffect(() => {
+        // @ts-ignore
+        invoke("get_executables").then((executables: Executable[]) => {
+            // @ts-ignore
+            setExecutables(executables);
+        });
+    }, []);
+
     return (
         <main className="container">
+            <h1>Launcher</h1>
+            <table>
+                <th>Name</th>
+                <th>Options</th>
 
+                {
+                    executables.map((executable: Executable) => (
+                        <tr key={executable.id}>
+                            <td>{executable.name}</td>
+                            <td>
+                                <button onClick={() => {
+                                    // @ts-ignore
+                                    invoke("launch_executable", {path: executable.path});
+                                }}>Launch</button>
+                            </td>
+                        </tr>
+                    ))
+                }
+            </table>
         </main>
     );
 }
