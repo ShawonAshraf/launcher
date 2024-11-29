a023a72978c6447fcc86e863e4bb6fa0aa72f42e
Shawon Ashraf <7804029+ShawonAshraf@users.noreply.github.com>
2024-11-29 12:53:25 +0100

    tbdoy nest

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {invoke} from "@tauri-apps/api/core";
 import "../App.css";
 // @ts-ignore
-import { Executable } from "../Types.ts";
+import {Executable} from "../Types.ts";
 
 export function Table() {
     const [executables, setExecutables] = useState([]);
@@ -19,37 +19,41 @@ export function Table() {
 
     return (
         <table className={"exe-table"}>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Options</th>
-            <th>Status ID</th>
-            {
-                executables.map((executable: Executable) => (
-                    <tr key={executable.id}>
-                        <td>{executable.id}</td>
-                        <td>{executable.name}</td>
-                        <td>
-                            <button onClick={() => {
-                                // @ts-ignore
-                                invoke("run_executable", {path: executable.path}).then((statusId: number) => {
-                                    setStatusId(statusId);
-                                });
-                            }}>Launch
-                            </button>
-
-                            <button onClick={() => {
-                                invoke("delete_executable", {id: executable.id}).then(() => {
+            <tbody>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Actions</th>
+                    <th>Status</th>
+                </tr>
+                {
+                    executables.map((executable: Executable) => (
+                        <tr key={executable.id}>
+                            <td>{executable.id}</td>
+                            <td>{executable.name}</td>
+                            <td>
+                                <button onClick={() => {
                                     // @ts-ignore
-                                    setExecutables(executables.filter((e: Executable) => e.id !== executable.id));
-                                });
-                            }}
-                            >Delete
-                            </button>
-                        </td>
-                        <td>{statusId}</td>
-                    </tr>
-                ))
-            }
+                                    invoke("run_executable", {path: executable.path}).then((statusId: number) => {
+                                        setStatusId(statusId);
+                                    });
+                                }}>Launch
+                                </button>
+
+                                <button onClick={() => {
+                                    invoke("delete_executable", {id: executable.id}).then(() => {
+                                        // @ts-ignore
+                                        setExecutables(executables.filter((e: Executable) => e.id !== executable.id));
+                                    });
+                                }}
+                                >Delete
+                                </button>
+                            </td>
+                            <td>{statusId}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
         </table>
     );
 
